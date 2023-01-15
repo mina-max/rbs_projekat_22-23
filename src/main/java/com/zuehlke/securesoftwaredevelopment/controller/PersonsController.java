5d82e3c927c7fe9d0d68bc8e835906ef6f28be2f
Mirjana Radosavljević <57013840+mina-max@users.noreply.github.com>
2023-01-15 23:31:29 +0100

    Initial commit

---
+++ src/main/java/com/zuehlke/securesoftwaredevelopment/controller/PersonsController.java
@@ -0,0 +1,94 @@
+package com.zuehlke.securesoftwaredevelopment.controller;
+
+import com.zuehlke.securesoftwaredevelopment.config.AuditLogger;
+import com.zuehlke.securesoftwaredevelopment.config.DatabaseAuthenticationProvider;
+import com.zuehlke.securesoftwaredevelopment.config.SecurityUtil;
+import com.zuehlke.securesoftwaredevelopment.domain.Person;
+import com.zuehlke.securesoftwaredevelopment.domain.User;
+import com.zuehlke.securesoftwaredevelopment.repository.PersonRepository;
+import com.zuehlke.securesoftwaredevelopment.repository.UserRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpSession;
+import java.sql.SQLException;
+import java.util.List;
+
+@Controller
+
+public class PersonsController {
+
+    private static final Logger LOG = LoggerFactory.getLogger(PersonsController.class);
+    private static final AuditLogger auditLogger = AuditLogger.getAuditLogger(PersonRepository.class);
+
+    private final PersonRepository personRepository;
+    private final UserRepository userRepository;
+
+    public PersonsController(PersonRepository personRepository, UserRepository userRepository) {
+        this.personRepository = personRepository;
+        this.userRepository = userRepository;
+    }
+
+    @GetMapping("/persons/{id}")
+    public String person(@PathVariable int id, Model model, HttpSession session) {
+        if(!SecurityUtil.hasPermission("VIEW_PERSON") && SecurityUtil.getCurrentUser().getId() != id)
+        {
+            throw new AccessDeniedException("Forbidden");
+        }
+        String csrf = session.getAttribute("CSRF_TOKEN").toString();
+        model.addAttribute("CSRF_TOKEN", session.getAttribute("CSRF_TOKEN"));
+        model.addAttribute("person", personRepository.get("" + id));
+        return "person";
+    }
+
+    @GetMapping("/myprofile")
+    @PreAuthorize("hasAuthority('VIEW_MY_PROFILE')")
+    public String self(Model model, Authentication authentication, HttpSession session) {
+        String csrf = session.getAttribute("CSRF_TOKEN").toString();
+        model.addAttribute("CSRF_TOKEN", session.getAttribute("CSRF_TOKEN"));
+        User user = (User) authentication.getPrincipal();
+        model.addAttribute("person", personRepository.get("" + user.getId()));
+        return "person";
+    }
+
+    @DeleteMapping("/persons/{id}")
+    public ResponseEntity<Void> person(@PathVariable int id) {
+        personRepository.delete(id);
+        userRepository.delete(id);
+
+        return ResponseEntity.noContent().build();
+    }
+
+    @PostMapping("/update-person")
+    public String updatePerson(Person person, HttpSession session, @RequestParam("csrfToken") String csrfToken) throws
+            AccessDeniedException {
+        String csrf = session.getAttribute("CSRF_TOKEN").toString();
+        if (!csrf.equals(csrfToken)) {
+            AuditLogger.getAuditLogger(DatabaseAuthenticationProvider.class).audit("Unauthorized attempt to update person!");
+            throw new AccessDeniedException("Forbidden");
+        }
+        personRepository.update(person);
+        AuditLogger.getAuditLogger(DatabaseAuthenticationProvider.class).audit("Person with the id " + person.getId() + " has been updated!");
+        return "redirect:/persons/" + person.getId();
+    }
+
+    @GetMapping("/persons")
+    @PreAuthorize("hasAuthority('VIEW_PERSONS_LIST')")
+    public String persons(Model model) {
+        model.addAttribute("persons", personRepository.getAll());
+        return "persons";
+    }
+
+    @GetMapping(value = "/persons/search", produces = "application/json")
+    @ResponseBody
+    public List<Person> searchPersons(@RequestParam String searchTerm) throws SQLException {
+        return personRepository.search(searchTerm);
+    }
+}
